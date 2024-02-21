22fbdbfa2648316aaa44e56a4daf08d409080eb3
Silfr-Coder <157309052+Silfr-Coder@users.noreply.github.com>
2024-02-21 16:27:23 +0000

    Updated CSS

--- e-commerce-site/src/App.js
+++ e-commerce-site/src/App.js
@@ -3,9 +3,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div className="header-box">Logo Box</div>
+          <div className="header-box">Username</div>
+          <div className="header-box">Credit: £0.00</div>
+          <div className="header-box">Hello, Username</div>
+          <div className="header-box">Box 5</div>
+        </header>
+      </div>
+      <div className="main-container">Here is an AWESOME E-COMMERCE site</div>
+    </>
   );
 }
 
